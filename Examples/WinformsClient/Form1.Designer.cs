4a48942fd31e409ae060bc044af0517de33fc9a5
JKorf <jankorf91@gmail.com>
2017-11-24 15:48:25 +0100

    Added winform example project

---
+++ Examples/WinformsClient/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace WinformsClient
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(105, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "BTC-ETH Last price:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 30);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(94, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "BTC-ETH Volume:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Examples/WinformsClient/Form1.cs
@@ -0,0 +1,47 @@
+﻿using Bittrex.Net;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinformsClient
+{
+    public partial class Form1 : Form
+    {
+        private BittrexSocketClient socketClient;
+        
+        public Form1()
+        {
+            InitializeComponent();
+
+            Load += LoadDone;
+        }
+
+        private void LoadDone(object sender, EventArgs e)
+        {
+            Task.Run(() =>
+            {
+                socketClient = new BittrexSocketClient();
+                socketClient.SubscribeToMarketDeltaStream("BTC-ETH", data =>
+                {
+                    UpdateLastPrice(data.Last);
+                });
+            });
+
+            Task.Run(() =>
+            {
+                using(var client = new BittrexClient())
+                {
+                    var result = client.GetMarketSummary("BTC-ETH");
+                    UpdateLastPrice(result.Result.Last);
+                    label2.Invoke(new Action(() => { label2.Text = "BTC-ETH Volume: " + result.Result.Volume; }));
+                }
+            });
+        }
+
+        private void UpdateLastPrice(decimal price)
+        {
+            label1.Invoke(new Action(() => { label1.Text = "BTC-ETH Last price: " + price; }));
+        }
+    }
+}
