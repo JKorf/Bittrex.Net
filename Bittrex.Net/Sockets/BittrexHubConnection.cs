c6088d5986f6e923d05ef2b446e123766664604c
matthias <matthias@andesia.org>
2018-01-03 02:45:55 +0100

    +Update: this works with win7 and .Net 4.5.2 and .Net Core2.0

--- Bittrex.Net/Sockets/BittrexHubConnection.cs
+++ Bittrex.Net/Sockets/BittrexHubConnection.cs
@@ -66,7 +66,7 @@ namespace Bittrex.Net.Sockets
         {
             var client = new HttpClientWithUserAgent();
             var autoTransport = new AutoTransport(client, new IClientTransport[] {
-                new LongPollingTransport(client),
+                //new LongPollingTransport(client),
                 new WebsocketCustomTransport(client),
             });
             connection.TransportConnectTimeout = new TimeSpan(0, 0, 10);
