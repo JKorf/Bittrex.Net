c49c2d28e1a1851da373c7e0729d663d666544b4
JKorf <jankorf91@gmail.com>
2018-03-27 13:50:48 +0200

    Added some code docs

--- Bittrex.Net/Objects/BittrexCandle.cs
+++ Bittrex.Net/Objects/BittrexCandle.cs
@@ -5,18 +5,39 @@ namespace Bittrex.Net.Objects
 {
     public class BittrexCandle
     {
+        /// <summary>
+        /// Open price of the candle
+        /// </summary>
         [JsonProperty("O")]
         public decimal Open { get; set; }
+        /// <summary>
+        /// High price of the candle
+        /// </summary>
         [JsonProperty("H")]
         public decimal High { get; set; }
+        /// <summary>
+        /// Low price of the candle
+        /// </summary>
         [JsonProperty("L")]
         public decimal Low { get; set; }
+        /// <summary>
+        /// Close price of the candle
+        /// </summary>
         [JsonProperty("C")]
         public decimal Close { get; set; }
+        /// <summary>
+        /// Volume of the candle
+        /// </summary>
         [JsonProperty("V")]
         public decimal Volume { get; set; }
+        /// <summary>
+        /// Base volume of the candle
+        /// </summary>
         [JsonProperty("BV")]
         public decimal BaseVolume { get; set; }
+        /// <summary>
+        /// Timestamp of the candle
+        /// </summary>
         [JsonProperty("T")]
         public DateTime Timestamp { get; set; }
     }
--- Bittrex.Net/Objects/BittrexStreamDeltas.cs
+++ Bittrex.Net/Objects/BittrexStreamDeltas.cs
@@ -4,7 +4,14 @@ namespace Bittrex.Net.Objects
 {
     public class BittrexStreamDeltas
     {
+        /// <summary>
+        /// Nounce
+        /// </summary>
         public long Nounce { get; set; }
+
+        /// <summary>
+        /// The current market summaries
+        /// </summary>
         public List<BittrexMarketSummary> Deltas { get; set; }
     }
 }
