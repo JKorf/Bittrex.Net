cff72866979410fefb94417c6f6e50454b450e86
vslee <dev-github@vslee.com>
2019-10-11 20:16:21 -0700

    Websockets: add FillId
    - added Id to BittrexStreamFill

--- Bittrex.Net/Objects/BittrexStreamFill.cs
+++ Bittrex.Net/Objects/BittrexStreamFill.cs
@@ -10,15 +10,20 @@ namespace Bittrex.Net.Objects
     /// </summary>
     public class BittrexStreamFill
     {
-        /// <summary>
-        /// Timestamp of the fill
-        /// </summary>
-        [JsonProperty("T"), JsonConverter(typeof(TimestampConverter))]
-        public DateTime Timestamp { get; set; }
-        /// <summary>
-        /// Quantity of the fill
-        /// </summary>
-        [JsonProperty("Q")]
+		/// <summary>
+		/// ID of the fill
+		/// </summary>
+		[JsonProperty("FI")]
+		public long Id { get; set; }
+		/// <summary>
+		/// Timestamp of the fill
+		/// </summary>
+		[JsonProperty("T"), JsonConverter(typeof(TimestampConverter))]
+		public DateTime Timestamp { get; set; }
+		/// <summary>
+		/// Quantity of the fill
+		/// </summary>
+		[JsonProperty("Q")]
         public decimal Quantity { get; set; }
         /// <summary>
         /// Rate of the fill
