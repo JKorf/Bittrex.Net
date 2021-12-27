5c77341665b7c31fa943b191b280d25acfe3de50
Jan Korf <jankorf91@gmail.com>
2021-12-27 16:13:41 +0100

    Updated comment

--- Bittrex.Net/Interfaces/Clients/SpotApi/IBittrexClientSpotApi.cs
+++ Bittrex.Net/Interfaces/Clients/SpotApi/IBittrexClientSpotApi.cs
@@ -24,7 +24,7 @@ namespace Bittrex.Net.Interfaces.Clients.SpotApi
         IBittrexClientSpotApiTrading Trading { get; }
 
         /// <summary>
-        /// Get the IExchangeClient for this client
+        /// Get the IExchangeClient for this client. This is a common interface which allows for some basic operations without knowing any details of the exchange.
         /// </summary>
         /// <returns></returns>
         IExchangeClient AsExchangeClient();
