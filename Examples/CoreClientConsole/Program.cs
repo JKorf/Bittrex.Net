ea263b227719830c09cffc55a7160deeeb0f0f14
Jkorf <jankorf91@gmail.com>
2022-02-16 16:07:30 +0100

    Updated examples

--- Examples/CoreClientConsole/Program.cs
+++ Examples/CoreClientConsole/Program.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Bittrex.Net;
+using Bittrex.Net.Clients;
 using Bittrex.Net.Objects;
 using CryptoExchange.Net.Authentication;
 using CryptoExchange.Net.Logging;
@@ -26,13 +27,13 @@ namespace CoreClientConsole
             using (var client = new BittrexClient())
             {
                 // public
-                var markets = await client.GetSymbolsAsync();
-                var currencies = await client.GetCurrenciesAsync();
-                var price = await client.GetTickerAsync("BTC-USDT");
-                var marketSummary = await client.GetSymbolSummaryAsync("BTC-USDT");
-                var marketSummaries = await client.GetSymbolSummariesAsync();
-                var orderbook = await client.GetOrderBookAsync("BTC-USDT");
-                var marketHistory = await client.GetTradeHistoryAsync("BTC-USDT");
+                var markets = await client.SpotApi.ExchangeData.GetSymbolsAsync();
+                var currencies = await client.SpotApi.ExchangeData.GetAssetsAsync();
+                var price = await client.SpotApi.ExchangeData.GetTickerAsync("BTC-USDT");
+                var marketSummary = await client.SpotApi.ExchangeData.GetSymbolSummaryAsync("BTC-USDT");
+                var marketSummaries = await client.SpotApi.ExchangeData.GetSymbolSummariesAsync();
+                var orderbook = await client.SpotApi.ExchangeData.GetOrderBookAsync("BTC-USDT");
+                var marketHistory = await client.SpotApi.ExchangeData.GetTradeHistoryAsync("BTC-USDT");
 
                 // private
                 // Commented to prevent accidental order placement
@@ -40,30 +41,30 @@ namespace CoreClientConsole
                 //    client.PlaceOrderAsync("BTC-USDT", OrderSide.Sell, OrderTypeV3.Limit, TimeInForce.GoodTillCancelled, 1, 1);
                 //var orderInfo = client.GetOrderAsync(placedOrder.Data.Id);
                 //var canceledOrder = client.CancelOrderAsync(placedOrder.Data.Id);
-                var openOrders = await client.GetOpenOrdersAsync("BTC-USDT");
-                var orderHistory = await client.GetClosedOrdersAsync("BTC-USDT");
+                var openOrders = await client.SpotApi.Trading.GetOpenOrdersAsync("BTC-USDT");
+                var orderHistory = await client.SpotApi.Trading.GetClosedOrdersAsync("BTC-USDT");
 
-                var balance = await client.GetBalanceAsync("BTC");
-                var balances = await client.GetBalancesAsync();
-                var depositAddress = await client.GetDepositAddressAsync("BTC");
-                var withdraw = await client.WithdrawAsync("TEST", 1, "TEST", "TEST");
-                var withdrawHistory = await client.GetClosedWithdrawalsAsync();
-                var depositHistory = await client.GetClosedDepositsAsync();
+                var balance = await client.SpotApi.Account.GetBalanceAsync("BTC");
+                var balances = await client.SpotApi.Account.GetBalancesAsync();
+                var depositAddress = await client.SpotApi.Account.GetDepositAddressAsync("BTC");
+                var withdraw = await client.SpotApi.Account.WithdrawAsync("TEST", 1, "TEST", "TEST");
+                var withdrawHistory = await client.SpotApi.Account.GetClosedWithdrawalsAsync();
+                var depositHistory = await client.SpotApi.Account.GetClosedDepositsAsync();
             }
 
             // Websocket
             var socketClient = new BittrexSocketClient();
-            var subscription = socketClient.SubscribeToSymbolTickerUpdatesAsync("ETH-BTC", ticker =>
+            var subscription = socketClient.SpotStreams.SubscribeToTickerUpdatesAsync("ETH-BTC", ticker =>
             {
-                Console.WriteLine($"ETH-BTC: {ticker.Data.LastTradeRate}");
+                Console.WriteLine($"ETH-BTC: {ticker.Data.LastPrice}");
             });
 
-            var subscription2 = socketClient.SubscribeToOrderBookUpdatesAsync("ETH-BTC", 25, state =>
+            var subscription2 = socketClient.SpotStreams.SubscribeToOrderBookUpdatesAsync("ETH-BTC", 25, state =>
             {
                 // Order book update
             });
 
-            var subscription3 = socketClient.SubscribeToBalanceUpdatesAsync(data =>
+            var subscription3 = socketClient.SpotStreams.SubscribeToBalanceUpdatesAsync(data =>
             {
                 // Balance update
             });
--- Examples/WinformsClient/Form1.Designer.cs
+++ Examples/WinformsClient/Form1.Designer.cs
@@ -3,12 +3,12 @@
     partial class Form1
     {
         /// <summary>
-        /// Required designer variable.
+        ///  Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
         /// <summary>
-        /// Clean up any resources being used.
+        ///  Clean up any resources being used.
         /// </summary>
         /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
@@ -23,8 +23,8 @@
         #region Windows Form Designer generated code
 
         /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent()
         {
@@ -34,17 +34,17 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Location = new System.Drawing.Point(298, 198);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(105, 13);
+            this.label1.Size = new System.Drawing.Size(38, 15);
             this.label1.TabIndex = 0;
-            this.label1.Text = "BTC-ETH Last price:";
+            this.label1.Text = "label1";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -55,7 +55,6 @@
 
         #endregion
 
-        private System.Windows.Forms.Label label1;
+        private Label label1;
     }
 }
-
